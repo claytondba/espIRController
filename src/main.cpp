91118ca31164dc52df7f50f109aa4bf833c46d7e
Clayton Oliveira <claytondba@yahoo.com.br>
2017-08-23 14:17:44 -0300

    ..

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <PubSubClient.h>
 #include <ESP8266WiFi.h>
+#include <IRremoteESP8266.h>
 
 const char* SSID = "ssid";
 const char* PASSWORD = "pass";
@@ -13,18 +14,42 @@ const int BROKER_PORT = 8080;
 WiFiClient espClient;
 PubSubClient MQTT(espClient);
 
+bool MODO_COPIA = false; //Modo de cópia de codigos IR
+int RECV_PIN = 12; //PINO DIGITAL EM QUE O FOTORRECEPTOR ESTÁ CONECTADO - GPIO12 / PINO D6
+IRrecv irrecv(RECV_PIN); //VARIÁVEL DO TIPO IRrecv
+decode_results results;
+
+
 void initWiFi();
 void initSerial();
+void dump(decode_results *results);
+void mqtt_callback(char* topic, byte* payload, unsigned int length);
 
 void setup()
 {
+  initSerial();
 
- //Teste
+  if(MODO_COPIA)
+  {
+    irrecv.enableIRIn();
+  }
+  else
+  {
+    initWiFi();
+  }
 }
 
 void loop()
 {
-
+  if(MODO_COPIA)
+  {
+    if (irrecv.decode(&results))
+    {
+      Serial.println(results.value, HEX);
+      dump(&results);
+      irrecv.resume(); //RECEBE O PRÓXIMO VALOR
+    }
+  }
 }
 
 void initSerial() {
@@ -56,10 +81,65 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length) {
   if (message == "1") {
     Serial.println("1");
   }
-  else 
+  else
   {
     Serial.println(message);
   }
 
   Serial.flush();
+}
+
+void dump(decode_results *results) {
+  int count = results->rawlen;
+  if (results->decode_type == UNKNOWN) {
+    Serial.print("Unknown encoding: ");
+  }
+  else if (results->decode_type == NEC) {
+    Serial.print("Decoded NEC: ");
+  }
+  else if (results->decode_type == SONY) {
+    Serial.print("Decoded SONY: ");
+  }
+  else if (results->decode_type == RC5) {
+    Serial.print("Decoded RC5: ");
+  }
+  else if (results->decode_type == RC6) {
+    Serial.print("Decoded RC6: ");
+  }
+  else if (results->decode_type == PANASONIC) {
+    Serial.print("Decoded PANASONIC - Address: ");
+    Serial.print(results->panasonicAddress, HEX);
+    Serial.print(" Value: ");
+  }
+  else if (results->decode_type == LG) {
+    Serial.print("Decoded LG: ");
+  }
+  else if (results->decode_type == JVC) {
+    Serial.print("Decoded JVC: ");
+  }
+  else if (results->decode_type == AIWA_RC_T501) {
+    Serial.print("Decoded AIWA RC T501: ");
+  }
+  else if (results->decode_type == WHYNTER) {
+    Serial.print("Decoded Whynter: ");
+  }
+  Serial.print(results->value, HEX);
+  Serial.print(" (");
+  Serial.print(results->bits, DEC);
+  Serial.println(" bits)");
+  Serial.print("Raw (");
+  Serial.print(count, DEC);
+  Serial.print("): ");
+
+  for (int i = 1; i < count; i++) {
+    if (i & 1) {
+      Serial.print(results->rawbuf[i]*USECPERTICK, DEC);
+    }
+    else {
+      Serial.write('-');
+      Serial.print((unsigned long) results->rawbuf[i]*USECPERTICK, DEC);
+    }
+    Serial.print(" ");
+  }
+  Serial.println();
 }
